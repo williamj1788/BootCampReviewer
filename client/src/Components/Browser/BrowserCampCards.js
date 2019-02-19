916d9aa39bd6841efaad954138d6752801d91ba6
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-19 12:05:02 -0500

    step up browse to configure json data

--- client/src/Components/Browser/BrowserCampCards.js
+++ client/src/Components/Browser/BrowserCampCards.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import CampCard from './CampCard';
+var logos = require('C:/Users/deathkiller101x/Desktop/Projects/Websites/BootCampReviewer/client/src/Img/LambdaIcon.png');
+var campCardDatabase ={
+    CardData:[
+        {
+            logo: logos,
+            description: 'Lambda School 232330 week, immersive program that gives you the tools and training you need to launch your new career—from the comfort of your own home',
+            rating: 7,
+            cost: 30300,
+            duration: 38,
+        },
+        {
+            logo: logos,
+            description: 'Lambda School is a 30 week, immersive program that gives you the tools and training you need to launch your new career—from the comfort of your own home',
+            rating: 3,
+            cost: 30000,
+            duration: 2,
+        },
+        {
+            logo: logos,
+            description: 'Lambda School is a 30 week, immersive program that gives you the tools and training you need to launch your new career—from the comfort of your own home',
+            rating: 2,
+            cost: 3000,
+            duration: 30,
+        }
+    ]
+}
+
+
 
 export default class BrowseCampCard extends React.Component{
     
     render(){
+        console.log(campCardDatabase);
+        let Cards = campCardDatabase.CardData.map((item,i) =>{
+            return <CampCard history={this.props.history} data={item} key={i}/>
+        });
+
         return(
             <div className="card-container">
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
-                <CampCard history={this.props.history} />
+                {Cards}
             </div>
         )
     }
--- client/src/Components/Browser/CampCard.js
+++ client/src/Components/Browser/CampCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-var logo = require('C:/Users/deathkiller101x/Desktop/Projects/Websites/BootCampReviewer/client/src/Img/LambdaIcon.png');
+// var logo = require('C:/Users/deathkiller101x/Desktop/Projects/Websites/BootCampReviewer/client/src/Img/LambdaIcon.png');
 export default class CampCard extends React.Component{
     constructor(props){
         super(props);
@@ -15,13 +15,11 @@ export default class CampCard extends React.Component{
     render(){
         return(
             <div className="Campcard mx-3 my-5" onClick = {this.onClick}>
-                <img src={logo} alt="Bootcamp card logo" className="Campcard-img pt-3"></img>
-                <p className="Campcard-text">Lambda School is a 30 week, immersive program that gives 
-                you the tools and training you need to launch your new career—from the 
-                comfort of your own home</p>
-                <p className="Campcard-info"><span style={{fontWeight: "bold"}}>Rating: </span> 9 out of 10</p>
-                <p className="Campcard-info"><span style={{fontWeight: "bold"}}>Cost: </span>Special</p>
-                <p className="Campcard-info"><span style={{fontWeight: "bold"}}>Duration: </span>30 Weeks</p>
+                <img src={this.props.data.logo} alt="Bootcamp card logo" className="Campcard-img pt-3"></img>
+                <p className="Campcard-text">{this.props.data.description}</p>
+                <p className="Campcard-info"><span style={{fontWeight: "bold"}}>Rating: </span> {this.props.data.rating} out of 10</p>
+                <p className="Campcard-info"><span style={{fontWeight: "bold"}}>Cost: $</span>{this.props.data.cost}</p>
+                <p className="Campcard-info"><span style={{fontWeight: "bold"}}>Duration: </span>{this.props.data.duration} Weeks</p>
             </div>
         )
     }
